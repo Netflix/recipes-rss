714f4e240a13f5180c9b7aa32b7e135e41e25f55
Prasanna Padmanabhan <ppadmanabhan@netflix.com>
2015-02-26 14:36:07 -0800

    Catch ParserConfigurationException

--- rss-middletier/src/main/java/com/netflix/recipes/rss/manager/RSSManager.java
+++ rss-middletier/src/main/java/com/netflix/recipes/rss/manager/RSSManager.java
@@ -148,8 +148,8 @@ public class RSSManager implements HealthCheckHandler {
         RSS rssItems = null;
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         String FEATURE = "http://apache.org/xml/features/disallow-doctype-decl";
-        dbf.setFeature(FEATURE, true);        
         try {
+            dbf.setFeature(FEATURE, true);
             DocumentBuilder db = dbf.newDocumentBuilder();
             try {
                 InputSource is = new InputSource(new StringReader(rss));
